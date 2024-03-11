f2b0e4b216b676fc7ea25ec9c9f38309144a4d74
Calvin Brosch <cbrosch0@gmail.com>
2024-03-11 17:23:19 -0400

    Added the footer, minus proper  font handling
    CSS derived from the cs.ucf.edu website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import CsFooter from './components/CsFooter';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <CsFooter/>
     </div>
   );
 }
---
+++ frontend/src/components/CsFooter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/CsFooter.css";
+
+function CsFooter() {
+  return (
+    <div className="FooterBody">
+      <div>
+        <div className="FooterTitle">
+          <p>DEPARTMENT OF CS, CECS, UCF</p>
+        </div>
+
+        <div className="FooterText">
+          <p>
+            4328 Scorpius Street, Orlando, Florida, 32816-2362 | 407.823.3957
+            <br />© University of Central Florida
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CsFooter;
