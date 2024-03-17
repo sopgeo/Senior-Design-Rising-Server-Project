1eb1a68267e9b66d2fb8530f844c0c343b5fa334
ryanglatour <ryanglatour@gmail.com>
2024-03-17 16:33:30 -0400

    Setup backend server

---
+++ backend/models/index.js
@@ -0,0 +1,18 @@
+const dbConfig = require("../db.config");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER,
+dbConfig.PASSWORD, {
+	host: dbConfig.HOST,
+	dialect: dbConfig.dialect,
+	operationsAliases: false,
+});
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.projects = require("./projectmodel")(sequelize, Sequelize)
+
+
+module.exports = db;
