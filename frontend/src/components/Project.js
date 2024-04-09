cbb0ca32c6f18d5d650e91083605bf06427cd8b2
Calvin Brosch <cbrosch0@gmail.com>
2024-04-09 05:49:15 -0400

    More getters, online PDF, set up for easy loading

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -5,14 +5,23 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "../css/Project.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Project() {
+
+  const urlInfo = new URLSearchParams(window.location.search);
+  var projectId;
+  if (urlInfo.has("projectId")) {
+      projectId = urlInfo.get("projectId");
+  } else {
+      projectId = "";
+  }
+
   const [projectName, setProjectName] = useState("Placeholder name");
   const [description, setDescription] = useState(
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis enim eu sollicitudin posuere. Integer bibendum molestie sem quis pretium. Pellentesque neque leo, volutpat tristique ante in, elementum commodo turpis. Phasellus eleifend vulputate rutrum. Nunc sed lacus a nibh volutpat tempor. Etiam dignissim, lacus eget commodo dictum, quam elit aliquet ex, non auctor leo risus ultricies ipsum. In vulputate sapien rhoncus urna bibendum imperdiet. Pellentesque varius risus id sapien hendrerit cursus."
   );
-  const [show, setShow] = useState("https://wwf.org");
+  const [show, setShow] = useState("https://media.team254.com/2023/10/90bc07c6-2023-Tech-Binder-V6.pdf");
   const [tags, setTags] = useState([
     "test",
     "this is another tag",
@@ -59,6 +68,22 @@ function Project() {
     }
   }
 
+  function getName(){
+    return projectName;
+  }
+
+  function getDescription(){
+    return description;
+  }
+
+  function getPDF(){
+    return show;
+  }
+
+  useEffect(() => {
+    // May be needed to run functions in the return if they give loading issues
+  }, []);
+
   return (
     <>
       <Container className="PageBody">
@@ -67,7 +92,7 @@ function Project() {
         <br />
 
         <Row>
-          <h1 className="Title">{projectName}</h1>
+          <h1 className="Title">{getName()}</h1>
         </Row>
 
         <Row>
@@ -75,7 +100,7 @@ function Project() {
             <div className="Details">
               <figure>{displayTags()}</figure>
 
-              <p class="Details">{description}</p>
+              <p class="Details">{getDescription()}</p>
             </div>
           </Col>
           <Col auto>
@@ -87,7 +112,9 @@ function Project() {
         </Row>
 
         <Row classname="PDF">
-          <iframe src={show} width={"75%"} height={"1000px"} />
+          <object data={getPDF()} type="application/pdf" width="100%" height="1000px">
+            WHYYYYYY
+          </object>
         </Row>
         <br />
         <br />
