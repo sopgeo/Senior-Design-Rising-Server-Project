0064625710722e8c050dbef3398328e65f48a327
sopgeo <sophiageorgieva@knights.ucf.edu>
2024-05-03 11:54:51 -0400

    home page at 85%
    still need to: fix text box over image to get smaller as page gets smaller, add images, add relevant text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,27 @@ import ProjectPage from './pages/ProjectPage'
 import Upload from './components/Upload';
 import Home from './components/Home';
 import Project from './components/Project';
+import Navbar from './components/Navbar';
+import Footer from './components/CsFooter.js';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
+    
     <div className="App">
+      <Navbar/>
       <BrowserRouter>
         <Routes>
           <Route path='/ab' index element={<ProjectPage/>} />
-          <Route path='/home' index element={<Home/>} />
+          <Route path='/' index element={<Home/>} />
           <Route path='/upload' index element={<Upload/>} />
           <Route path='/project' index element={<Project/>} />
 
         </Routes>
       </BrowserRouter>
-
-      {/* Uncomment the below line to view and edit upload page */}
-      {/* <Upload/> */}
+      <Footer/>
     </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,10 +4,57 @@ import "../css/Home.css";
 function Home() {
   return (
     <div className="home-page">
+      {/* header! */}
+      <div className="header">
+        <div className="header-logo">
+          Computer Science Senior Design Projects
+        </div>
+        <img className="img" src={require('../images/l3harris.jpg')} alt="L3Harris"/>
+      </div>
+
+    
         <div className="white-rect">
-            <h1>About</h1>
-            <p>Loreum ipsum</p>
-            {/* <img>insert image here</img> */}
+          <div class="about">
+            <h1 id="about-header">About</h1>
+            <hr class="hr1"/>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          {/* <img className="about-img" src={require('../images/some-logo.png')}/> */}
+          <img className="about-img" src='../images/some-logo.png'/>
+        </div>
+
+        <div className="yellow-rect">
+          {/* <img className="pres-img" src={require('../images/some-logo.png')}/> */}
+          <img className="pres-img" src='../images/some-logo.png'/>
+          <div class="presentations">
+            <h1 id="pres-header">Presentations</h1>
+            <hr class="hr2"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+
+        <div className="black-rect">
+          <div class="sponsors">
+            <h1 id="sponsor-header">Sponsors</h1>
+            <hr className="hr3"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          {/* <img className="sponsor-img" src={require('../images/some-logo.png')}/> */}
+          <img className="sponsor-img" src='../images/some-logo.png'/>
+        </div>
+
+        <div className="white-rect2">
+          <div class="sd1">
+            <h1 id="sd1-header">Senior Design 1</h1>
+            <hr class="hr4"/>
+            <p class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+
+          <div class="sd2">
+            <h1 id="sd2-header">Senior Design 2</h1>
+            <hr class="hr5"/>
+            <p class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
         </div>
     </div>
   );
