5dee044e52f080f783048d9c56b4100ffc9f26c1
sopgeo <sophiageorgieva@knights.ucf.edu>
2024-05-08 11:19:41 -0400

    drag area section aesthetics done (not functional)

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -3,7 +3,8 @@ import "../css/Upload.css";
 
 function Upload() {
   return (
-    <div className="upload-page">
+      <div className="upload-page">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <div className="upload-header">
             <h1>Upload your project</h1>
             <h3>Enter metadata about your project and submit it for admin approval</h3>
@@ -52,9 +53,18 @@ function Upload() {
 
             <div className="line2">
                 <div className="tech-doc-field">
-                    <label id="tech-doc">Upload your technical document</label>
+                <label id="tech-doc">Upload your technical document</label>
                     <br/>
-                    <input type="text" id="upload-project-placeholder"/>
+                    <div className="drag-area">
+                        <span className="header">Drag and drop a file here</span>
+                        <span className="header">or <span class="button">select a file</span> <span className="header2"> from your computer </span> </span>
+                        <span class="support">Supports: PDF only</span>
+                        <div className="icon">
+                            <i class="fas fa-upload"></i>
+                            {/* <i class="fas fa-cloud-upload"></i> */}
+                        </div>
+                    </div>
+                    {/* <input type="file" id="myfile" name="myfile" multiple></input> */}
                 </div>
 
                 <div className="proj-desc-field">
