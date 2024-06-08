d14de4ebd6d17a73dc6ca3f53b23b6f2ecc76e0c
ryanglatour <ryanglatour@gmail.com>
2024-06-08 13:20:16 -0400

    Prepare dbconfig for server

---
+++ backend/db.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') })
+
+module.exports = {
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: "sdwebportal",
+    dialect: "mysql"
+    };
