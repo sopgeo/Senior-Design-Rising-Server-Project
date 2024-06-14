7406bdf17e8ccfcf369047088cdc9b2508149420
brandonl0ra7 <brandonlora07@gmail.com>
2024-06-14 17:25:26 -0400

    Create groupcontroller.js
    Created a group controller file which has basic functions for parsing the csv data

---
+++ backend/controllers/groupcontroller.js
@@ -0,0 +1,48 @@
+//will need to install csv-parse node module
+const fs = require('fs'); //look into using ES modules
+const csvParser = require('csv-parser');
+
+//Returns a promise and if the promise is resolved, then it returns the data within the csv
+//The data is returned as an array of objects, with each object containing the information for one row 
+function parseCSV( path ){
+    const results = [];
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path)
+            .pipe(csvParser())
+            .on('data', (data) => results.push(data))
+            .on('end', () => resolve(results))
+            .on('error', (error) => reject(error));
+    });
+}
+
+//Returns an object that has arrays of student names with keys that are the group names
+async function parseGroupInfo( path ){
+
+    const groups = {};
+
+    try { 
+
+        const data = await parseCSV( path ); //awaiting promise
+
+        data.forEach((row) => {
+            const studentName = row['name'];
+            const groupName = row['group_name'];
+
+            //checking if group exists
+            if (!groups[groupName]) {
+                groups[groupName] = [];
+            }
+
+            //add student name to group
+            groups[groupName].push(studentName);
+        });
+
+        return groups; 
+
+    } catch (error) {
+
+        console.error('Error parsing group CSV', error);
+    }
+
+}
