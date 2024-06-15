b2f704b8aee10d85a3f8d3d65dcea1d4d50f56c8
ryanglatour <ryanglatour@gmail.com>
2024-06-15 18:13:49 -0400

    Creating/Deleting Tags (still can't assign them)

---
+++ backend/controllers/tagcontroller.js
@@ -0,0 +1,36 @@
+const db = require("../models/")
+
+const Tag = db.tags
+const TagMap = db.tagmap
+
+exports.createTag = async (req, res) => {
+    try {
+      const newTag = Tag.create({
+        name: req.body.name
+      })
+  
+      res.send({ message: 'Tag created'})
+    }
+    catch (error) {
+      res.status(500).json({error: error.message, message: "Error occurred creating tag"})
+    }
+  }
+
+  exports.deleteTag = async (req, res) => {
+    try {
+        const result = await Tag.destroy({
+            where: {
+                tag_id: req.body.tag_id
+            }
+        })
+
+        if (result > 0) {
+            res.send({ message: `Tag deleted`})
+          } else {
+            res.send({ message: `No tag found with tag_id${req.body.tag_id}`});
+          }
+    }
+    catch (error) {
+        res.status(500).json({error: error.message, message: "Error occurred deleting user"})
+    }
+}
