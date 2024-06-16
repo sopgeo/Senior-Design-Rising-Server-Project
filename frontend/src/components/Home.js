72ad8502600e1faabc54ca2f80e0ced62bb9f001
sopgeo <sophiageorgieva@knights.ucf.edu>
2024-06-16 14:22:00 -0400

    latest version + npm install react-select

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -17,7 +17,7 @@ function Home() {
           <div class="about">
             <h1 id="about-header">About</h1>
             <hr class="hr1"/>
-            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="text">Welcome to the Computer Science Senior Design web portal. This is a central hub for exploring final-year projects in Computer Science department at UCF. Guest access is limited to viewing and searching projects but as a UCF senior design student, you have the opportunity to upload your team’s project. Dive into a collection of the latest ideas fostered by the innovative community of CS@UCF.</p>
           </div>
           {/* <img className="about-img" src={require('../images/some-logo.png')}/> */}
           <img className="about-img" src='../images/some-logo.png'/>
@@ -29,7 +29,7 @@ function Home() {
           <div class="presentations">
             <h1 id="pres-header">Presentations</h1>
             <hr class="hr2"/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Towards the end of Senior Design 2, students showcase their projects in a 15–20-minute presentation as a testament to their dedicated research and collaboration. This is presented to a committee of three judges that witness the culmination of the seniors’ creativity and efforts.</p>
           </div>
         </div>
 
@@ -37,7 +37,7 @@ function Home() {
           <div class="sponsors">
             <h1 id="sponsor-header">Sponsors</h1>
             <hr className="hr3"/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>At the beginning of the SD cycle, sponsors pitch their projects to eager students. These projects include a range of topics from artificial intelligence, frontend design, cyber security, software engineering, database systems, and more, and often projects include a mix of many topics. Teams get the opportunity to pursue a Fall-Spring, Spring-Summer, or Spring-Fall path.</p>
           </div>
           {/* <img className="sponsor-img" src={require('../images/some-logo.png')}/> */}
           <img className="sponsor-img" src='../images/some-logo.png'/>
@@ -47,15 +47,16 @@ function Home() {
           <div class="sd1">
             <h1 id="sd1-header">Senior Design 1</h1>
             <hr class="hr4"/>
-            <p class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="text1">Senior Design 1 (SD1) marks the beginning of a rewarding journey. During this semester, students are pitched projects by sponsors and usually get placed in their first or second pick, which is how teams are selected. Going forward, biweekly TA check-in meetings are required and so are meetings with the project sponsor(s). At the end, a final detailed technical document with roughly 30 pages of contribution from each member is required to pass SD1. Each student writes about the work they have done and describes technical details about the project.</p>
           </div>
 
           <div class="sd2">
             <h1 id="sd2-header">Senior Design 2</h1>
             <hr class="hr5"/>
-            <p class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="text2">Building upon the groundwork of SD1, students continue to refine their projects and address challenges. Lectures are minimal but expectations soar. At the end of SD2, teams present their projects to a panel of judges and are evaluated on technical proficiency and presentation skills, among other criteria. Upon completion, each team has illuminated the path to new possibilities, inspiring students and judges around them, as they are sent off to embark on their next adventures.</p>
           </div>
         </div>
+        
     </div>
   );
 }
--- frontend/src/components/TagsInput.js
+++ frontend/src/components/TagsInput.js
@@ -1,30 +1,69 @@
 import { useState } from 'react'
+import Select from "react-select";
 import "../css/TagsInput.css";
 
+const options = [
+    {value: "HTML", label: "HTML"},
+    {value: "Machine Learning", label: "Machine Learning"},
+    {value: "Artificial Intelligence", label: "Artificial Intelligence"},
+    {value: "Figma", label: "Figma"},
+    {value: "Java", label: "Java"},
+    {value: "JavaScript", label: "JavaScript"},
+    {value: "Unity", label: "Unity"},
+    {value: "Object Oriented Programming", label: "Object Oriented Programming"},
+    {value: "Web Development", label: "Web Development"},
+    {value: "Tensorflow", label: "Tensorflow"},
+    {value: "C#", label: "C#"},
+    {value: "C++", label: "C++"},
+    {value: "Python", label: "Python"},
+    {value: "Research", label: "Research"},
+    {value: "Internet of Things", label: "Internet of Things"},
+    {value: "Game Development", label: "Game Development"},
+    {value: "Mechanical Engineering", label: "Mechanical Engineering"},
+    {value: "Computer Science", label: "Computer Science"},
+    {value: "Electrical Engineering", label: "Electrical Engineering"},
+    {value: "CSS", label: "CSS"},
+    {value: "Sensors", label: "Sensors"},
+    {value: "Detection/", label: "AWS"},
+    {value: "Biology", label: "Biology"},
+    {value: "Genomics", label: "Genomics"},
+    {value: "Robotics", label: "AWS"},
+    {value: "VR", label: "VR"},
+    {value: "Linguistics", label: "Linguistics"},
+    {value: "MySQL", label: "MySQL"},
+    {value: "MongoDB", label: "MongoDB"},
+    {value: "AWS", label: "AWS"},
+    {value: "Scientific Research", label: "Scientific Research"},
+    {value: "Computer Vision", label: "Computer Vision"},
+    {value: "UI/UX", label: "UI/UX"}
+]
+
 function TagsInput(){
-    const [tags, setTags] = useState([])
+    const [selectedOptions, setSelectedOptions] = useState([])
 
-    function handleKeyDown(e){
-        if(e.key !== 'Enter') return
-        const value = e.target.value
-        if(!value.trim()) return
-        setTags([...tags, value])
-        e.target.value = ''
-    }
+    const handleChange = (selectedOption) => {
+        setSelectedOptions(selectedOption);
+    };
 
-    function removeTag(index){
-        setTags(tags.filter((el, i) => i !== index))
-    }
+    const customStyles = {
+        option: (provided, state) => ({
+          ...provided,
+          fontSize: 14,
+          color: 'black',
+          borderColor: 'black',
+          backgroundColor: 'black',
+          backgroundColor: state.isSelected ? 'black' : 'white', // Change background color for selected options
+        }),
+      };
 
     return (
         <div className="tags-input-container">
-            { tags.map((tag, index) => (
-                <div className="tag-item" key={index}>
-                    <span className="text">{tag}</span>
-                    <span className="close" onClick={() => removeTag(index)}>&times;</span>
-                </div>
-            )) }
-            <input onKeyDown={handleKeyDown} type="text" className="tags-input" placeholder="Type and press enter" />
+            <Select
+                options={options}
+                value={selectedOptions}
+                onChange={handleChange}
+                isMulti={true}
+            />
         </div>
     )
 }
