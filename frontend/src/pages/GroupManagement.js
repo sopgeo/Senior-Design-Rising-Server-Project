02762cda3288661573ee46583ed8a14f4ac66483
brandonl0ra7 <158063745+brandonl0ra7@users.noreply.github.com>
2024-06-28 11:26:44 -0400

    Started the renderSemesters function
    Started the renderSemsesters function and added functions to render a single semester and a single group.

--- frontend/src/pages/GroupManagement.js
+++ frontend/src/pages/GroupManagement.js
@@ -15,13 +15,51 @@ function GroupManagement() {
     // May be needed to run functions in the return if they give loading issues
   }, []);
 
-  function renderSemester(semInfo) {
+  //Returns the table which contains all of the groups for a given semester
+  function createSingleSemester(semInfo){
+
+  }
+
+  //Will render every table from all the semesters
+  function renderSemesters(semInfo) {
+
+    let semesterTables; //return variable that holds all of the info
+
+    let arr = new Array();
+    let date = new Date();
+    for (let i = 0; i <= date.getFullYear() - 2016; i++) {
+      arr = arr.concat([
+        "Spring " + (i + 2016).toString(),
+        "Summer " + (i + 2016).toString(),
+        "Fall " + (i + 2016).toString(),
+      ]);
+    }
+
+    arr = arr.concat(" ");
+    arr = arr.reverse();
+    console.log(arr);
+
+
+    for(let i = 0; i <= arr.length(); i++){
+
+      //api search for group from a given semester
+
+      let semInfo = {
+        semester : "placeholder",
+        year: "placeholder"
+      };
+
+      semesterTables += createSingleSemester(semInfo);
+    }
+
     return(
       <div className="SemesterBar">AAAAA</div>
     )
   }
+  
+  function renderGroup(groupInfo) {
 
-  function renderGroup(groupInfo) {}
+  }
 
   return (
     <>
@@ -33,7 +71,7 @@ function GroupManagement() {
         <h1 className="Title">Group Management</h1>
         <br />
 
-        {renderSemester()}
+        {renderSemesters()}
       </div>
       <CsFooter />
     </>
