539261092ae5d987ea9510fc242f20bf6c550508
Calvin Brosch <cbrosch0@gmail.com>
2024-07-25 12:53:39 -0400

    Update Search.js

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -264,7 +264,7 @@ function Search() {
         {
           header: "Title",
           cell: ({ row }) => (
-            <>{row.original.documents > 0 ? "🗎 " + row.original.name : row.original.name} </>
+            <>{row.original.files.length > 0 ? "🗎 " + row.original.name : row.original.name} </>
           ),
         },
         {
