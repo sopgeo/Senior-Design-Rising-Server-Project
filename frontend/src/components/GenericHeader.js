bcb104c82b29e73453f09da40337b75beff74e8c
sopgeo <sophiageorgieva@knights.ucf.edu>
2024-07-28 21:38:44 -0400

    deleted some old code

--- frontend/src/components/GenericHeader.js
+++ frontend/src/components/GenericHeader.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CSS from "../css/GenericHeader.css";
 
 export function GenericHeader({ background }) {
   const logout = () => {
@@ -34,11 +33,6 @@ export function GenericHeader({ background }) {
     }
     }
 
-  function showSidebar() {
-    const sidebar = CSS.querySelector(".sidebar")
-    // sidebar.style.display = 'flex'
-  }
-
   function NavBar() {
     let user = localStorage.getItem("user")
     let css = "background";
@@ -47,11 +41,6 @@ export function GenericHeader({ background }) {
       return (
         <div className={css}>
           <div className="navbar">
-            <div className="home"><a className="navItem" href="/">Home</a></div>
-              <a className="navItem" href="/search">Projects</a>
-              {/* <li onClick={showSidebar()}><a className="navItem"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li> */}
-            </div>
-            <div className="sidebar">
             <div className="home"><a className="navItem" href="/">Home</a></div>
               <a className="navItem" href="/search">Projects</a>
             </div>
@@ -75,13 +64,6 @@ export function GenericHeader({ background }) {
             <a className="navItem" href="/search">Projects</a>
             <a className="navItem" href="/upload">Upload</a>
             <a className="navItem" href="/groupManagement">Manage</a>
-            {/* <a className="navItem"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a> */}
-          </div>
-          <div className="sidebar">
-            <div className="home"><a className="navItem" href="/">Home</a></div>
-            <a className="navItem" href="/search">Projects</a>
-            <a className="navItem" href="/upload">Upload</a>
-            <a className="navItem" href="/groupManagement">Manage</a>
           </div>
         </div>
       );
@@ -89,12 +71,6 @@ export function GenericHeader({ background }) {
       return (
         <div className={css}>
           <div className="navbar">
-            <div className="home"><a className="navItem" href="/">Home</a></div>
-            <a className="navItem" href="/search">Projects</a>
-            <a className="navItem" href="/upload">Upload</a>
-            {/* <a className="navItem"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a> */}
-          </div>
-          <div className="sidebar">
             <div className="home"><a className="navItem" href="/">Home</a></div>
             <a className="navItem" href="/search">Projects</a>
             <a className="navItem" href="/upload">Upload</a>
